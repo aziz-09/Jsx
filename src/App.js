5073ba59a4a416c0688c3ce3c6ea2e88dac70512
unknown <azizlakhal415@gmail.com>
2020-06-16 12:00:43 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,38 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import myImage from "./srcimg.png"
+import  Mohamed from "./component/test"
+
+
 
 function App() {
   return (
     <div className="App"> 
   
-    <div style={{
-      border:'solid 1px black',
-      maxWidth:"100vw"
-    }}>  
-    
-       
-       <div>
-    	<h1 className="title red">  
-      <p> Your name here </p> 
+       <div className = "center" >
+    	<h1 className="title"> 
+      <p> Aziz </p> 
       </h1>  
       </div>
- 
-        
-    <img src={myImage}/> 
- 
+      <br /> 
+  
+    <img className="editimg" src={myImage}/>  <br />
   	<img src={"/publicImg.png"}/>  
  
    
  
-    <video width="320" height="240" controls>  
+    <video className ="edit"  width="320" height="240" controls>  
  
-        <source src="myVideo.mp4" type="video/mp4"/> 
+     <source src="myVideo.mp4" type="video/mp4"/> 
  
     </video> 
  
     </div>
-    </div>
+    
   );
 }
 
---
+++ src/component/test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./test.css"
+
+ function test(){
+    return (
+        <div className="hello">
+        <h1>hello aziz</h1>    
+        </div>
+    )
+}
+export default test
